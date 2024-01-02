01f337ef56a116ca5b5a98d65b3a57f0d583f221
Jack Sankey <jack.sankey@gmail.com>
2024-01-02 17:01:25 -0500

    Update package.py

--- package.py
+++ package.py
@@ -1,7 +1,5 @@
 import zipfile, os
 
 path = os.path.join('assettocorsa','apps','python','ac-clock','ac-clock.py')
-
 version = input('Version? ').strip()
-
 zip_file = zipfile.ZipFile('ac-clock v'+version+'.zip', 'w')
